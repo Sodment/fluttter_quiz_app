6670d4f72d67258c1b6c98eb338340c0e2f22387
Sodment <sodment@gmail.com>
2021-07-14 11:39:40 +0200

    Add simple buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import './question.dart';
+import './answer.dart';
 
 void main() => runApp(MyApp());
 
@@ -22,8 +23,33 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     var questions = [
-      'Co ty odpierdalasz?',
-      "whast your fav animal?",
+      {
+        'questionText': 'What\'s your favourite color?',
+        'answers': [
+          'Black',
+          'Red',
+          'White',
+          'Green',
+        ],
+      },
+      {
+        'questionText': 'What\'s your favourite animal?',
+        'answers': [
+          'Dog',
+          'Cat',
+          'Hamster',
+          'Bird',
+        ],
+      },
+      {
+        'questionText': 'What\'s your favorite italian dish?',
+        'answers': [
+          'Spaghetti',
+          'Lasagna',
+          'Pizza',
+          'Italian sticks',
+        ],
+      },
     ];
     return MaterialApp(
       home: Scaffold(
@@ -32,19 +58,10 @@ class _MyAppState extends State<MyApp> {
         ),
         body: Column(
           children: <Widget>[
-            Question(questions[_questionIndex]),
-            RaisedButton(
-              onPressed: _answerQuestion,
-              child: Text("Answer 1"),
-            ),
-            RaisedButton(
-              onPressed: _answerQuestion,
-              child: Text("Answer 2"),
-            ),
-            RaisedButton(
-              onPressed: _answerQuestion,
-              child: Text("Answer 3"),
-            ),
+            Question(questions[_questionIndex]['questionText'].toString()),
+            for (var answer
+                in (questions[_questionIndex]['answers'] as List<String>))
+              Answer(_answerQuestion, answer),
           ],
         ),
       ),
